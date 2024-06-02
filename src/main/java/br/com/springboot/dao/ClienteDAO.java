07e8811906d716fee6ee7edea06f70e000b24be8
Werricsson <werricsson.santos@hotmail.com>
2024-06-02 11:37:03 -0300

    Alterando os imports para o javax

--- src/main/java/br/com/springboot/dao/ClienteDAO.java
+++ src/main/java/br/com/springboot/dao/ClienteDAO.java
@@ -5,9 +5,9 @@ import java.util.List;
 import org.springframework.stereotype.Repository;
 
 import br.com.springboot.model.Cliente;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import jakarta.persistence.Query;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 @Repository
 public class ClienteDAO implements CRUD<Cliente, Long>{
