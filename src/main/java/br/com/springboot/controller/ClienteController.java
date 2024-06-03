e4fe15f05779ec174d4038272b6941e9b2b953ec
Werricsson <werricsson.santos@hotmail.com>
2024-06-03 19:51:36 -0300

    Mapeando a requisição http do método POST

--- src/main/java/br/com/springboot/controller/ClienteController.java
+++ src/main/java/br/com/springboot/controller/ClienteController.java
@@ -3,6 +3,7 @@ package br.com.springboot.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -21,5 +22,11 @@ public class ClienteController {
 	public ModelAndView novo(ModelMap model) {
 		model.addAttribute("cliente", new Cliente());
 		return new ModelAndView("/cliente/formulario", model);
+	}
+	
+	@RequestMapping(value = "", method = RequestMethod.POST)
+	public String salva(@ModelAttribute Cliente cliente) {
+		bo.insere(cliente);
+		return "/cliente/formulario";
 	}
 }
