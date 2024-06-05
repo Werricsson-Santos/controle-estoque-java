e9487189bed09eda52cfe9867aa8380f63adb019
Werricsson <werricsson.santos@hotmail.com>
2024-06-04 22:49:47 -0300

    Mapeando a validação dos dados

--- src/main/java/br/com/springboot/controller/ClienteController.java
+++ src/main/java/br/com/springboot/controller/ClienteController.java
@@ -3,21 +3,24 @@ package br.com.springboot.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.springboot.bo.ClienteBO;
 import br.com.springboot.model.Cliente;
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/clientes")
 public class ClienteController {
 	
 	@Autowired
-	private ClienteBO bo;
+	private ClienteBO clienteBO;
 	
 	@RequestMapping(value = "/novo", method = RequestMethod.GET)
 	public ModelAndView novo(ModelMap model) {
@@ -26,35 +29,40 @@ public class ClienteController {
 	}
 	
 	@RequestMapping(value = "", method = RequestMethod.POST)
-	public String salva(@ModelAttribute Cliente cliente) {
-		bo.insere(cliente);
-		return "redirect:/clientes";
+	public String salva(@Valid @ModelAttribute Cliente cliente, BindingResult result, RedirectAttributes attr) {
+		if (result.hasErrors()) {
+			return "/cliente/formulario";
+		}
+		else {
+			clienteBO.insere(cliente);
+			return "redirect:/clientes";
+		}
 	}
 	
 	@RequestMapping(value = "", method = RequestMethod.GET)
 	public ModelAndView lista(ModelMap model) {
-		model.addAttribute("clientes", bo.lista());
+		model.addAttribute("clientes", clienteBO.lista());
 		return new ModelAndView("/cliente/lista", model);
 	}
 	
 	@RequestMapping(value = "/edita/{id}", method = RequestMethod.GET)
 	public ModelAndView edita(@PathVariable("id") Long id, ModelMap model) {
-		model.addAttribute("cliente", bo.pesquisaPeloId(id));
+		model.addAttribute("cliente", clienteBO.pesquisaPeloId(id));
 		return new ModelAndView("/cliente/formulario", model);
 	}
 	
 	
 	@RequestMapping(value = "/inativa/{id}", method = RequestMethod.GET)
 	public String inativa(@PathVariable("id") Long id) {
-		Cliente cliente = bo.pesquisaPeloId(id);
-		bo.inativa(cliente);
+		Cliente cliente = clienteBO.pesquisaPeloId(id);
+		clienteBO.inativa(cliente);
 		return "redirect:/clientes";
 	}
 	
 	@RequestMapping(value = "/ativa/{id}", method = RequestMethod.GET)
 	public String ativa(@PathVariable("id") Long id) {
-		Cliente cliente = bo.pesquisaPeloId(id);
-		bo.ativa(cliente);
+		Cliente cliente = clienteBO.pesquisaPeloId(id);
+		clienteBO.ativa(cliente);
 		return "redirect:/clientes";
 	}
 }
