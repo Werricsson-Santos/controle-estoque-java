59fd39732631d923e9afd7e8b4667f1d56d77e80
Werricsson <werricsson.santos@hotmail.com>
2024-06-05 13:56:42 -0300

    Inserindo mapeamento da rota para a página inicial

--- src/main/java/br/com/springboot/controller/AplicacaoController.java
+++ src/main/java/br/com/springboot/controller/AplicacaoController.java
@@ -1,5 +1,13 @@
 package br.com.springboot.controller;
 
-public class AplicacaoController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
+public class AplicacaoController {
+	@RequestMapping(value = {"/", ""}, method = RequestMethod.GET)
+	public String index() {
+		return "index";
+	}
 }
